38a23e45ded836dc52cf0add8eba24c4288cad57
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2023-01-06 17:33:41 -0300

    agrego base de datos cursos

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+// desestructuracion infocursos, para importarlo directamente
+const { infoCursos } = require('./cursos.js');
+// console.log(infoCursos);
+// routing sitio principal
+app.get('/', (req, res) => {
+    res.send('Mi primer servidor con Express. Cursos');
+});
+// routing cursos
+app.get('/api/cursos', (req, res) => {
+    res.send(JSON.stringify(infoCursos));
+});
+// routing cursos de programacion
+app.get('/api/cursos/programacion', (req, res) => {
+    res.send(JSON.stringify(infoCursos.programacion))
+})
+
+// parametros url programacion
+app.get('/api/cursos/programacion/:lenguaje', (req, res) => {
+    const lenguaje = req.params.lenguaje;
+    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje);
+    if (resultados.length === 0) {
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
+    }
+    res.send(JSON.stringify(resultados));
+});
+
+app.get('/api/cursos/programacion/:lenguaje/:nivel', (req, res) => {
+    const lenguaje = req.params.lenguaje;
+    const nivel = req.params.nivel;
+    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje && curso.nivel === nivel);
+    if (resultados.length === 0) {
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`)
+    }
+    res.send(JSON.stringify(resultados));
+});
+
+// routing cursos de matematicas
+app.get('/api/cursos/matematicas', (req, res) => {
+    res.send(JSON.stringify(infoCursos.matematicas))
+})
+// parametros url matematicas
+app.get('/api/cursos/matematicas/:tema', (req, res) => {
+    const tema = req.params.tema;
+    const resultados = infoCursos.matematicas.filter(curso => curso.tema === tema);
+    if (resultados.length === 0) {
+        return res.status(404).send(`No se encontraron cursos de ${tema}`);
+    }
+    res.send(JSON.stringify(resultados));
+});
+
+
+const PUERTO = process.env.PORT || 3000;
+app.listen(PUERTO, () => {
+    console.log(`El servidor esta escuchando en el puerto ${PUERTO}`);
+});
